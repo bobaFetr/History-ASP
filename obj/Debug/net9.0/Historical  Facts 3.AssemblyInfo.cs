e14cafa0320d4d81d9c63c4ceeef770f0da17f6b
Boris Petkov <boris.v.bobi.petkoff@gmail.com>
2025-05-14 13:09:13 +0300

    In this version the  registration andlogin pages cause problems(they temporarely comented)

--- obj/Debug/net9.0/Historical  Facts 3.AssemblyInfo.cs
+++ obj/Debug/net9.0/Historical  Facts 3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Historical  Facts 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc30865f05846fd0c982264f663e1bdeec8fd30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb057b5690d6531481675c9aba874d463012c61d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Historical  Facts 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Historical  Facts 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
