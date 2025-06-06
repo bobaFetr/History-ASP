9afbd338f231c46fbd71bc24d629b6ea6ace3eec
Boris Petkov <boris.v.bobi.petkoff@gmail.com>
2025-06-06 13:05:32 +0300

    6/6/25

--- obj/Debug/net9.0/Historical  Facts 3.AssemblyInfo.cs
+++ obj/Debug/net9.0/Historical  Facts 3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Historical  Facts 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f96df62f053d33d1565af266acc04a59ee21380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8676b51aad743a68b1c5ea5d16f8411c2b3c5f8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Historical  Facts 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Historical  Facts 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
